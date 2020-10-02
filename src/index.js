9186b735c550d5cbaf5312196b34055178d84ce0
devalmanu <manuilova-1986@mail.ru>
2020-10-02 14:11:26 +0300

    feat: add rusilt task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// let min = [1, 2, 3, 4];
+// let max = [1, 2, 3, 4];
+// let arr = [1, 2, 3, 4];
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function (array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    return Math.min(...array);
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || !array.length) {
+        return 0;
+    }
+    return array.reduce((sum, item) => sum + item, 0) / array.length;
+
 }
